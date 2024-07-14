21abde64b9bc54fd78cf5d082f24bd8c279655da
Vikas-garg-tiet26 <vgarg1_be22@thapar.edu>
2024-07-14 11:59:41 +0530

    themeing default

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:expense_trcker/models/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 130, 81, 245));
 void main() {
   runApp(const ExpenseApp());
 }
@@ -11,9 +12,17 @@ class ExpenseApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return (const MaterialApp(
+    return (MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme:kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+          
+        )
+      ),
       debugShowCheckedModeBanner: false,
-      home: Expenses(),
+      home:const Expenses(),
     ));
   }
 }
--- lib/models/expenses_list.dart
+++ lib/models/expenses_list.dart
@@ -1,20 +1,21 @@
 import 'package:expense_trcker/models/expense_trcker.dart';
-import 'package:expense_trcker/models/expenses.dart';
 import 'package:expense_trcker/models/widgets/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses,required this.onRemoveExpense});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<ExpenseTrcker> expenses;
-  final void Function(ExpenseTrcker expense)onRemoveExpense;
+  final void Function(ExpenseTrcker expense) onRemoveExpense;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
       itemBuilder: (context, index) => Dismissible(
           key: ValueKey(expenses[index]),
-          onDismissed:(direction) => onRemoveExpense(expenses[index]), child: ExpenseItem(expenses[index])),
+          onDismissed: (direction) => onRemoveExpense(expenses[index]),
+          child: ExpenseItem(expenses[index])),
     );
   }
 }
